2198411f543174df67525c3e07c77cbe9462caa2
ds5678 <jrpritts7@gmail.com>
2021-01-14 13:56:56 -0500

    patched resources.load

--- src/Patches.cs
+++ src/Patches.cs
@@ -50,20 +50,35 @@ namespace AssetLoader
 
     // Hinterland load assets by calling Resources.Load which ignores external AssetBundles
     // so we need to patch Resources.Load to redirect specific calls to load from the AssetBundle instead
-    //[HarmonyPatch(typeof(Resources), "Load", new Type[] { typeof(string) })]
-    //internal class Resources_Load
-    //{
-    //    internal static bool Prefix(string path, ref UnityEngine.Object __result)
-    //    {
-    //        if (!ModAssetBundleManager.IsKnownAsset(path))
-    //        {
-    //            return true;
-    //        }
+    [HarmonyPatch]
+    internal class Resources_Load
+    {
+        static MethodBase TargetMethod()
+        {
+            MethodInfo[] methods = typeof(Resources).GetMethods();
+            foreach(MethodInfo m in methods)
+            {
+                if(m.Name == "Load" && m.ReturnType == typeof(UnityEngine.Object) && !m.IsGenericMethod && m.GetParameters().Length == 1)
+                {
+                    return m;
+                }
+                
+            }
+            //AccessTools.Method(typeof(Resources)., "Load", new Type[] { typeof(string) });
+            
+            return null;
+        }
+        internal static bool Prefix(string path, ref UnityEngine.Object __result)
+        {
+            if (!ModAssetBundleManager.IsKnownAsset(path))
+            {
+                return true;
+            }
 
-    //        __result = ModAssetBundleManager.LoadAsset(path);
-    //        return false;
-    //    }
-    //}
+            __result = ModAssetBundleManager.LoadAsset(path);
+            return false;
+        }
+    }
 
     public class SaveAtlas : MonoBehaviour
     {
